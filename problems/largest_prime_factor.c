b2995a716984334353754331a006106263ec3432
smplcd <asmplcd@gmail.com>
2023-07-29 19:34:51 +0000

    Solve Problem 3 - Largest Prime Factor
    This one took way too long...
    
    The MAXPRIMEFN is the maximum number of Prime factors that a
    uint_64 can hold. (2^64) I wanted to refactor the code to have a
    function that'd return a list of prime factors of a given prime but it's
    not garuanteed to be an int64.
    
    Perhaps a union with all possible int types?
    
    A better program would ask for an input instead of having a hardcoded
    value.

---
+++ problems/largest_prime_factor.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define MAXPRIMEFN 64
+
+int main(int argc, char *argv[]){
+    uint64_t prime = 600851475143;
+
+    int primef[MAXPRIMEFN];
+    int *pprimef = primef;
+
+    int n = 2;
+
+    while (pow(n, 2) < prime){
+        if (prime % n == 0){
+            *pprimef++ = n;
+            prime /= n;
+        }
+        else{
+            n++;
+        }
+    }
+    *pprimef = prime;
+
+    printf("Biggest Prime Factor: %d\n", *pprimef);
+    return 0;
+}
+
