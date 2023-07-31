79c1c7c3dd429da8a0bc28949bdc4c53129c2e76
smplcd <asmplcd@gmail.com>
2023-07-31 17:18:35 +0000

    Fix the name of lcm
    It didn't make any sense so I looked up what it actually stood for...

--- problems/5 - smallest_multiple.c
+++ problems/5 - smallest_multiple.c
@@ -2,17 +2,17 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define UPPER 10
+#define UPPER 20
 #define LOWER 1
 
 uint64_t greatest_common_denominator(uint64_t x, int y);
-uint64_t lowest_common_multiplier(uint64_t x, int y);
+uint64_t least_common_multiple(uint64_t x, int y);
 
 int main(int argc, char *argv[]){
 	uint64_t product = LOWER;
 
 	for(int i=fmax(2 ,LOWER); i<UPPER; i++){
-		product = lowest_common_multiplier(product, i);
+		product = least_common_multiple(product, i);
 	}
 
 	printf("Largest Number evenly divisible by all numbers between %d and %d: %llu\n", LOWER, UPPER, product);
@@ -29,6 +29,6 @@ uint64_t greatest_common_denominator(uint64_t x, int y){
 	return n;
 }
 
-uint64_t lowest_common_multiplier(uint64_t x, int y){
+uint64_t least_common_multiple(uint64_t x, int y){
 	return ((x * y) / greatest_common_denominator(x, y));
 }
